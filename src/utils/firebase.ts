d6bee6aa4ea66119567d83f71d04ecd762c85609
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-09-12 10:07:06 -0300

    MASTER: handle constants

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { Auth, getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { Firestore, getFirestore } from 'firebase/firestore';
+import {
+  VITE_FIREBASE_APIKEY,
+  VITE_FIREBASE_AUTHDOMAIN,
+  VITE_FIREBASE_DATABASEURL,
+  VITE_FIREBASE_PROJECTID,
+  VITE_FIREBASE_STORAGEBUCKET,
+  VITE_FIREBASE_MESSAGINGSENDERID,
+  VITE_FIREBASE_APPID,
+  VITE_FIREBASE_MEASUREMENTID,
+} from './constants';
 
 const setupFirebase = (): {
   db: Firestore;
@@ -8,14 +18,14 @@ const setupFirebase = (): {
   googleAuthProvider: GoogleAuthProvider;
 } => {
   const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-    appId: import.meta.env.VITE_FIREBASE_APPID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+    apiKey: VITE_FIREBASE_APIKEY,
+    authDomain: VITE_FIREBASE_AUTHDOMAIN,
+    databaseURL: VITE_FIREBASE_DATABASEURL,
+    projectId: VITE_FIREBASE_PROJECTID,
+    storageBucket: VITE_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: VITE_FIREBASE_MESSAGINGSENDERID,
+    appId: VITE_FIREBASE_APPID,
+    measurementId: VITE_FIREBASE_MEASUREMENTID,
   };
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);

bfc131e3c0d4e2f6170d5f22df0998e1e7dbd4fe
Nicolas Diz <diz.nicolasamor@gmail.com>
2023-10-31 10:24:23 -0300

    MASTER: create src/mock/movie.mock.ts

---
+++ src/mock/movie.mock.ts
@@ -0,0 +1,13 @@
+export default {
+  movieId: 12345,
+  title: 'The Shawshank Redemption',
+  releaseDate: '1994-09-23',
+  genre: ['Drama', 'Crime'],
+  director: 'Frank Darabont',
+  cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+  plot: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+  rating: 9.3,
+  duration: 142,
+  posterUrl: 'https://example.com/posters/shawshank_redemption.jpg',
+  trailerUrl: 'https://example.com/trailers/shawshank_redemption.mp4',
+};
